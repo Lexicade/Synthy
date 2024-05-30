ee77bea40d2849d593bc5ec6eeb485980809def2
Lexipherous <Jasonnlelong@gmail.com>
2024-05-30 12:23:52 +0100

    - updated pfp to discord.py framework

--- cogs/pfp.py
+++ cogs/pfp.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 import importlib
 import utils
@@ -8,33 +9,17 @@ importlib.reload(utils)
 class PFP(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        global extension_name
-        extension_name = "[PFP] "
 
-    @commands.defer(ephemeral=False)
-    @commands.command(
-        aliases=[],
-        application_command_meta=commands.ApplicationCommandMeta(
-            options=[
-                discord.ApplicationCommandOption(
-                    name="user",
-                    description="Post the users profile picture to chat.",
-                    type=discord.ApplicationCommandOptionType.user,
-                    required=True,
-                ),
-            ]
-        )
-    )
-    async def pfp(self, ctx, user: discord.Member):
-        """Post the users profile picture to chat."""
-        await ctx.send(content=user.avatar.with_format('jpg'))
+    @app_commands.command(name='pfp', description='Post the users profile picture to chat.')
+    async def pfp(self, interaction: discord.Interaction, user: discord.Member):
+        await interaction.response.send_message(content=user.avatar.with_format('jpg'))
 
 
-def setup(bot):
+async def setup(bot):
     print("INFO: Loading [PFP]... ", end="")
-    bot.add_cog(PFP(bot))
+    await bot.add_cog(PFP(bot))
     print("Done!")
 
 
-def teardown(bot):
+async def teardown(bot):
     print("INFO: Unloading [PFP]")
