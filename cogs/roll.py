46b5d01dc2ca440bab2167bf9f2b0ae7b9f0f3dd
Lexipherous <Jasonnlelong@gmail.com>
2024-05-30 12:21:18 +0100

    - renamed RollDice.py to roll.py - updated roll to discord.py framework

--- cogs/roll.py
+++ cogs/roll.py
@@ -1,65 +1,28 @@
 from discord.ext import commands
+from discord import app_commands
 import discord
 import re
 import random
-colours = {"default": 0,
-           "teal": 0x1abc9c,
-           "dark teal": 0x11806a,
-           "green": 0x2ecc71,
-           "dark green": 0x1f8b4c,
-           "blue": 0x3498db,
-           "dark blue": 0x206694,
-           "purple": 0x9b59b6,
-           "dark purple": 0x71368a,
-           "magenta": 0xe91e63,
-           "dark magenta": 0xad1457,
-           "gold": 0xf1c40f,
-           "dark gold": 0xc27c0e,
-           "orange": 0xe67e22,
-           "dark orange": 0xa84300,
-           "red": 0xe74c3c,
-           "dark red": 0x992d22,
-           "lighter grey": 0x95a5a6,
-           "dark grey": 0x607d8b,
-           "light grey": 0x979c9f,
-           "darker grey": 0x546e7a,
-           "blurple": 0x7289da,
-           "greyple": 0x99aab5}
+import ast
+import operator
 
 
-class RollDice(commands.Cog):
+class Roll(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        global extension_name
-        extension_name = "[Roll Dice] "
 
-    @commands.command(
-        aliases=[],
-        application_command_meta=commands.ApplicationCommandMeta(
-            options=[
-                discord.ApplicationCommandOption(
-                    name="arg",
-                    description="Roll whatever sided dice you need to roll.",
-                    type=discord.ApplicationCommandOptionType.string,
-                    required=False,
-                )
-            ],
-        )
-    )
-    async def roll(self, ctx, *, arg):
-        """Roll whatever sided dice you need to roll."""
+    @app_commands.command(name='roll', description='Roll whatever sided dice you need to roll.')
+    async def roll(self, interaction: discord.Interaction, arg: str):
         demo_roll = re.sub(r'[^\d+\-d-d]', " ", arg)
         print(f"`{demo_roll.strip()}`")
 
-        if demo_roll.strip() is not "":
+        if demo_roll.strip() != "":
             output = self.roll_dice(arg)
-            await ctx.send(content=output)
+            await interaction.response.send_message(content=output)
         else:
-            await ctx.send(content="I wasn't able to get a roll from that.")
+            await interaction.response.send_message(content="I wasn't able to get a roll from that.")
 
-    def roll_dice(self, str_input):
-            import ast
-            import operator
+    def roll_dice(self, str_input: str):
             _OP_MAP = {
                 ast.Add: operator.add,
                 ast.Sub: operator.sub,
@@ -168,11 +131,11 @@ class RollDice(commands.Cog):
                 return str_output
 
 
-def setup(bot):
-    print("INFO: Loading [RollDice]... ", end="")
-    bot.add_cog(RollDice(bot))
+async def setup(bot):
+    print("INFO: Loading [Roll]... ", end="")
+    await bot.add_cog(Roll(bot))
     print("Done!")
 
 
-def teardown(bot):
-    print("INFO: Unloading [RollDice]")
+async def teardown(bot):
+    print("INFO: Unloading [Roll]")
