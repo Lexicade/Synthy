d19904ee99b6f606f5b41849c356b8ddafb9969e
Lexipherous <Jasonnlelong@gmail.com>
2024-06-03 15:26:57 +0100

    - Added CustomSQL class for easier DB usage

--- utils.py
+++ utils.py
@@ -62,6 +62,69 @@ async def sql(sql: str, params: tuple):
     return sql_out
 
 
+class CustomSQL:
+    def __init__(self):
+        self.cur = None
+        self.con = None
+        self.sql = None
+        self.params = None
+        self.db_name = None
+        self.sql_data_output = None
+
+    async def _setup_sql(self):
+        self.con = psycopg2.connect(database=os.environ.get('db_name'),
+                                    user=os.environ.get('db_user'),
+                                    password=os.environ.get('db_pass'),
+                                    host=os.environ.get('db_host'),
+                                    port=os.environ.get('db_port'),
+                                    options=f'-c search_path={os.environ.get("db_schema")}')
+        self.cur = self.con.cursor(cursor_factory=extras.DictCursor)
+
+    async def _run_sql(self):
+        self.cur.execute(self.sql, self.params)
+        self.con.commit()
+
+    async def run_sql(self, sql: str, params: tuple):
+        await self._setup_sql()
+
+        self.sql = sql
+        self.params = params
+        await self._run_sql()
+
+        if self.sql.upper().startswith("SELECT"):
+            await self._process_sql()
+
+        await self._close_sql()
+        return self.sql_data_output
+
+    async def _process_sql(self):
+        self.sql_data_output = []
+        columns = [str(column[0]).lower() for column in self.cur.description]
+        rows = self.cur.fetchall()
+        for row in rows:
+            sql_row = dict(zip(columns, row))
+            self.sql_data_output.append(sql_row)
+            print(f"process_data: {self.sql_data_output}")
+
+    async def check_table(self, db_name: str):
+        await self._setup_sql()
+
+        self.db_name = db_name
+        await self._check_table()
+
+        await self._close_sql()
+        return self.sql_data_output
+
+    async def _check_table(self):
+        self.cur.execute("select exists(SELECT relname from pg_class where relname=%s);", (self.db_name,))
+        self.con.commit()
+        self.sql_data_output = self.cur.fetchone()[0]
+
+    async def _close_sql(self):
+        self.cur.close()
+        self.con.close()
+
+
 async def notice(title, message, colour="blue", footer=None):
     embed_colour = await get_colour(colour)
     emb = discord.Embed(title=title,
