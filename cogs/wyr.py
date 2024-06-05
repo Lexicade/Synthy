2f25dbc4eb37e5bc2de75c74918c44f2babba02b
Lexipherous <Jasonnlelong@gmail.com>
2024-06-05 13:43:51 +0100

    - Updated wyr to allow for deleting options

--- cogs/wyr.py
+++ cogs/wyr.py
@@ -3,7 +3,7 @@ import uuid
 
 import psycopg2
 from discord.ext import commands
-from discord import app_commands
+from discord import app_commands, ui
 import discord
 import importlib
 
@@ -17,6 +17,7 @@ importlib.reload(utils)
 
 class WYR(commands.GroupCog, name="wyr", description="Would you rather?"):
     def __init__(self, bot):
+        self.sql = CustomSQL()
         self.bot = bot
 
     @app_commands.command(name='addnew', description="Add a new would you rather option for this server.")
@@ -28,12 +29,20 @@ class WYR(commands.GroupCog, name="wyr", description="Would you rather?"):
 
     @app_commands.command(name='ask', description="Pose a would you rather.")
     async def ask(self, interaction: discord.Interaction):
-        sql = CustomSQL()
-        options = await sql.run_sql('SELECT wyr_option, added_by from database1.synthy.wyr WHERE guild_id = %s ORDER BY random() LIMIT 2;', (interaction.guild.id, ))
+        options = await self.sql.run_sql('SELECT wyr_option, added_by from database1.synthy.wyr WHERE guild_id = %s ORDER BY random() LIMIT 2;', (interaction.guild.id, ))
         emb = await utils.embed(interaction,
                                 "Would you rather",
                                 f":one: {options[0]['wyr_option']}\n<@{options[0]['added_by']}>\n\nOR\n\n:two: {options[1]['wyr_option']}\n<@{options[1]['added_by']}>")
         await interaction.response.send_message(embed=emb)
+
+    @app_commands.command(name='remove', description="Remove one of your options.")
+    async def remove(self, interaction: discord.Interaction):
+        raw_options = await self.sql.run_sql('SELECT wyr_option, added_by from database1.synthy.wyr WHERE guild_id = %s and added_by = %s ORDER BY wyr_option;', (interaction.guild.id, interaction.user.id))
+        options = []
+        for option in raw_options:
+            options.append(option['wyr_option'])
+        view = DropdownView(options=options)
+        await interaction.response.send_message('Pick your favourite colour:', view=view, ephemeral=True)
 
 
 async def setup(bot):
@@ -50,3 +59,25 @@ async def setup(bot):
 
 async def teardown(bot):
     print("INFO: Unloading [WYR]")
+
+
+class DropdownView(discord.ui.View):
+    def __init__(self, options: list):
+        super().__init__()
+        self.add_item(Dropdown(options_list=options))
+
+
+class Dropdown(discord.ui.Select):
+    def __init__(self, options_list: list):
+        options = []
+        for i in range(0, len(options_list)):
+            print(options_list[i])
+            options.append(discord.SelectOption(label=options_list[i]))
+
+        super().__init__(placeholder='Choose one of your options to remove...', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        sql = CustomSQL()
+        await sql.run_sql('DELETE FROM database1.synthy.wyr WHERE guild_id = %s and added_by = %s and wyr_option = %s;',
+                          (interaction.guild.id, interaction.user.id, self.values[0]))
+        await interaction.response.send_message(f'Removed: `{self.values[0]}`', ephemeral=True)
